3d159d8285745ff2111b99893e114f9caf23d900
Andrey Kataev <kata3v.andrey@yandex.ru>
2025-07-12 13:24:50 +0300

    Прототип гейтвея (#1)

---
+++ clients/comment.py
@@ -0,0 +1,9 @@
+import grpc
+import stubs.comment_pb2_grpc, stubs.comment_pb2
+
+_channel = grpc.insecure_channel("localhost:7777")
+_stub = stubs.comment_pb2_grpc.CommentServiceStub(_channel)
+
+def create_comment_rpc(mod_id: int, author_id: int, text: str) -> stubs.comment_pb2.CreateCommentResponse:
+    req = stubs.comment_pb2.CreateCommentRequest(mod_id=mod_id, author_id=author_id, text=text)
+    return _stub.CreateComment(req)
