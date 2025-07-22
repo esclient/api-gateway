85cdfdc596ec8878a1590189842b9d99cdaf1d27
Samuel <samueldanielyan@gmail.com>
2025-07-22 20:47:41 +0200

    Delete Comment Mutation

--- resolvers/mutation_resolver.py
+++ resolvers/mutation_resolver.py
@@ -1,8 +1,14 @@
 from ariadne import MutationType
 from clients.comment import create_comment_rpc
+from clients.comment import delete_comment_rpc
 mutation = MutationType()
 
 @mutation.field("createComment")
 def resolve_create_comment(_, info, input):
     resp = create_comment_rpc(input["mod_id"], input["author_id"], input["text"])
     return str(resp.comment_id)
+
+@mutation.field("deleteComment")
+def resolve_delete_comment(_, info, input):
+    resp = delete_comment_rpc(input["comment_id"])
+    return bool(resp.success)
