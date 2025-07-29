d22bf83d017b951b15f1d8f20e9a97e40f0bec85
Samuel <samueldanielyan@gmail.com>
2025-07-29 22:51:16 +0200

    get comments query (#10)

--- src/gateway/resolvers/query_resolver.py
+++ src/gateway/resolvers/query_resolver.py
@@ -1,7 +1,19 @@
 from ariadne import QueryType
+from gateway.clients.comment import get_comments_rpc
 query = QueryType()
 
 
-@query.field("test")
-def resolve_test(_, info):
-    return True
+@query.field("getComments")
+def resolve_get_comments(_, info, input):
+    resp = get_comments_rpc(int(input["mod_id"]))
+    print(resp)
+    return [
+        {
+            "id": item.id,
+            "text": item.text,
+            "author_id": item.author_id,
+            "created_at": item.created_at
+
+        }
+        for item in resp.comments
+    ]
