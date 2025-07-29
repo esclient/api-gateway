3ce8461fc589825fb3fbbc176cbd8d145fd5d2fc
Samuel <samueldanielyan@gmail.com>
2025-07-29 20:14:08 +0200

    Update

--- src/gateway/resolvers/query_resolver.py
+++ src/gateway/resolvers/query_resolver.py
@@ -5,6 +5,15 @@ query = QueryType()
 
 @query.field("getComments")
 def resolve_get_comments(_, info, input):
-    resp = get_comments_rpc(input["mod_id"])
+    resp = get_comments_rpc(int(input["mod_id"]))
     print(resp)
-    return str(resp)
+    return [
+        {
+            "id": item.id,
+            "text": item.text,
+            "author_id": item.author_id,
+            "created_at": item.created_at
+
+        }
+        for item in resp.comments
+    ]
