f1709ffd7d71af892db3e8d3aafd64dd55c51032
Andrey Kataev <kata3v.andrey@yandex.ru>
2025-08-24 12:49:41 +0300

    Перенес стабы

--- src/gateway/stubs/rating_pb2.pyi
+++ src/gateway/stubs/rating_pb2.pyi
@@ -1,8 +1,24 @@
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Optional as _Optional
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
+
+class Rate(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = ()
+    RATE_UNSPECIFIED: _ClassVar[Rate]
+    RATE_1: _ClassVar[Rate]
+    RATE_2: _ClassVar[Rate]
+    RATE_3: _ClassVar[Rate]
+    RATE_4: _ClassVar[Rate]
+    RATE_5: _ClassVar[Rate]
+RATE_UNSPECIFIED: Rate
+RATE_1: Rate
+RATE_2: Rate
+RATE_3: Rate
+RATE_4: Rate
+RATE_5: Rate
 
 class RateModRequest(_message.Message):
     __slots__ = ("mod_id", "author_id", "rate")
@@ -11,8 +27,8 @@ class RateModRequest(_message.Message):
     RATE_FIELD_NUMBER: _ClassVar[int]
     mod_id: int
     author_id: int
-    rate: int
-    def __init__(self, mod_id: _Optional[int] = ..., author_id: _Optional[int] = ..., rate: _Optional[int] = ...) -> None: ...
+    rate: Rate
+    def __init__(self, mod_id: _Optional[int] = ..., author_id: _Optional[int] = ..., rate: _Optional[_Union[Rate, str]] = ...) -> None: ...
 
 class RateModResponse(_message.Message):
     __slots__ = ("rate_id",)
