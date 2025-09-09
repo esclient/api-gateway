d5698ca3825ebf4ef211d891a485fa8fbed659e3
Andrey Kataev <kata3v.andrey@yandex.ru>
2025-09-09 15:02:50 +0300

    Фикс лишних правок

--- src/gateway/resolvers/query_resolver.py
+++ src/gateway/resolvers/query_resolver.py
@@ -1,20 +1,26 @@
 from ariadne import QueryType
 from gateway.clients.comment import get_comments_rpc
+from gateway.clients.mod import get_mod_download_link_rpc
+
 query = QueryType()
 
 
 @query.field("getComments")
 def resolve_get_comments(_, info, input):
     resp = get_comments_rpc(int(input["mod_id"]))
-    print(resp)
     return [
         {
             "id": item.id,
             "text": item.text,
             "author_id": item.author_id,
             "created_at": item.created_at,
-            "edited_at": item.edited_at if item.edited_at != 0 else None
-            
+            "edited_at": item.edited_at if item.edited_at != 0 else None,
         }
         for item in resp.comments
     ]
+
+
+@query.field("getModDownloadLink")
+def resolve_get_mod_download_link(_, info, input):
+    resp = get_mod_download_link_rpc(int(input["mod_id"]))
+    return resp.link_url
