7ffd0858462913f08cfd5b7749fab1e2c0a493bf
MystiSs <113998288+MystiSs@users.noreply.github.com>
2025-09-23 23:23:37 +0300

    Добавлен и внёдрен хелпер-конвертер str(ID) в int(ID) (#34)

--- src/gateway/resolvers/query/comment.py
+++ src/gateway/resolvers/query/comment.py
@@ -1,11 +1,13 @@
 from ariadne import ObjectType
 from gateway.clients.comment import get_comments_rpc
+from ...helpers.id_helper import validate_and_convert_id
 
 comment_query = ObjectType("CommentQuery")
 
 @comment_query.field("getComments")
 def resolve_get_comments(_, info, input):
-    resp = get_comments_rpc(int(input["mod_id"]))
+    mod_id = validate_and_convert_id(input["mod_id"], "mod_id")
+    resp = get_comments_rpc(mod_id)
     return [
         {
             "id": item.id,
--- src/gateway/resolvers/query/mod.py
+++ src/gateway/resolvers/query/mod.py
@@ -1,9 +1,11 @@
 from ariadne import ObjectType
 from gateway.clients.mod import get_mod_download_link_rpc
+from ...helpers.id_helper import validate_and_convert_id
 
 mod_query = ObjectType("ModQuery")
 
 @mod_query.field("getModDownloadLink")
 def resolve_get_mod_download_link(_, info, input):
-    resp = get_mod_download_link_rpc(int(input["mod_id"]))
+    mod_id = validate_and_convert_id(input["mod_id"], "mod_id")
+    resp = get_mod_download_link_rpc(mod_id)
     return resp.link_url
