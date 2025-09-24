e640481192512b8828555ddaf286376ebcb7b743
Andrey Kataev <kata3v.andrey@yandex.ru>
2025-09-24 15:14:18 +0300

    Интеграция типизации

--- src/gateway/resolvers/query/mod.py
+++ src/gateway/resolvers/query/mod.py
@@ -1,11 +1,19 @@
 from ariadne import ObjectType
 from gateway.clients.mod import get_mod_download_link_rpc
-from ...helpers.id_helper import validate_and_convert_id
+from gateway.helpers.id_helper import validate_and_convert_id
+from pydantic import BaseModel, field_validator
+
+class GetModDownloadLinkInput(BaseModel):
+    mod_id: int
+
+    @field_validator("mod_id", mode="before")
+    def _mod_id(cls, v):
+        return validate_and_convert_id(v, "mod_id")
 
 mod_query = ObjectType("ModQuery")
 
 @mod_query.field("getModDownloadLink")
-def resolve_get_mod_download_link(_, info, input):
-    mod_id = validate_and_convert_id(input["mod_id"], "mod_id")
-    resp = get_mod_download_link_rpc(mod_id)
+def resolve_get_mod_download_link(_, info, input) -> str:
+    data = GetModDownloadLinkInput.model_validate(input)
+    resp = get_mod_download_link_rpc(data.mod_id)
     return resp.link_url
